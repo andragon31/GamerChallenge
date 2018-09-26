a1b8c6f8ba9b516019d80fbbd1e4b448c6f73313
andragon31 <oscarcamarena31@hotmail.com>
2018-09-26 15:07:14 -0600

    bd

--- web/php/conexion.php
+++ web/php/conexion.php
@@ -1,6 +1,6 @@
 <?php
     $link = mysqli_connect ("db4free.net:3306", "gcuser", "gamer123");
-	mysqli_select_db( $link, "bdgamer" );
+	mysqli_select_db( $link, "bdgamer");
     if(mysqli_connect_errno())
     {
         printf("conexion fallida", mysqli_connect_error());
